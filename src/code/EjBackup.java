4dce4513412bb552be8c87724d440bf34d21df9e
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-09-29 11:48:20 +0200

    actualizacion de ejercicios backup con que solo copie si se ha modificado algun archivo

--- src/code/EjBackup.java
+++ src/code/EjBackup.java
@@ -8,30 +8,41 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 public class EjBackup {
-    static Scanner sc = new Scanner(System.in);
     public static void buckUpTotal() {
-        String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
-        File rutaOrigen = new File(rutaOrigenString);
-        String rutaDestinoString = Leer.pedirCadena("Introduce ruta archivo destino: ");
-        File rutaDestino = new File(rutaDestinoString);
-        copiaBuckUp(rutaOrigen, rutaDestino);
-    }
-    public static void buckUpIncremental() {
-        SimpleDateFormat formatoDate = new SimpleDateFormat("yy.MM.dd_HH.mm.ss");
-        String date = formatoDate.format(new Date());
-
-        String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
-        File rutaOrigen = new File(rutaOrigenString);
-        String rutaDestinoString = Leer.pedirCadena("Introduce ruta archivo destino: ");
-        File rutaDestino = new File(rutaDestinoString + date);
-        copiaBuckUp(rutaOrigen, rutaDestino);
-    }
-    public static void copiaBuckUp(File po, File pd) {
         try {
-            // Crear la carpeta de destino si no existe
+            String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
+            File rutaOrigen = new File(rutaOrigenString);
+            String rutaDestinoString = Leer.pedirCadena("Introduce ruta archivo destino: ");
+            File rutaDestino = new File(rutaDestinoString);
+
+            copiaBuckUp(rutaOrigen, rutaDestino);
+        } catch (IOException e) {
+            System.err.println("Error al realizar el backup: " + e.getMessage());
+        }
+    }
+
+    public static void buckUpIncremental() {
+        try {
+            SimpleDateFormat formatoDate = new SimpleDateFormat("yy.MM.dd_HH.mm.ss");
+            String date = formatoDate.format(new Date());
+
+            String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
+            File rutaOrigen = new File(rutaOrigenString);
+            String rutaDestinoString = Leer.pedirCadena("Introduce ruta carpeta de backups incrementales: ");
+            File rutaDestino = new File(rutaDestinoString + "/" + date);
+
+            copiaBuckUp(rutaOrigen, rutaDestino);
+        } catch (IOException e) {
+            System.err.println("Error al realizar el backup incremental: " + e.getMessage());
+        }
+    }
+
+    public static void copiaBuckUp(File po, File pd) throws IOException {
+        try {
+            // creo la carpeta de destino si no existe
             if (!pd.exists()) {
                 if (pd.mkdir()) {
-                    // Iterar sobre los archivos en la carpeta de origen
+                    // itero sobre los archivos en la carpeta de origen
                     File[] archivos = po.listFiles();
                     if (archivos != null) {
                         for (File archivo : archivos) {
--- src/code/EjBackupNIO.java
+++ src/code/EjBackupNIO.java
@@ -13,43 +13,52 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 public class EjBackupNIO {
-    static Scanner sc = new Scanner(System.in);
+
     public static void buckUpTotalNio() {
-        String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
-        Path rutaOrigen = Path.of(rutaOrigenString);
-        String rutaDestinoString = Leer.pedirCadena("Introduce ruta archivo destino: ");
-        Path rutaDestino = Path.of(rutaDestinoString);
-        copiaBuckUp(rutaOrigen, rutaDestino);
+        try {
+            String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
+            Path rutaOrigen = Path.of(rutaOrigenString);
+            String rutaDestinoString = Leer.pedirCadena("Introduce ruta archivo destino: ");
+            Path rutaDestino = Path.of(rutaDestinoString);
+            copiaBuckUp(rutaOrigen, rutaDestino);
+        } catch (IOException e) {
+            System.err.println("Error al realizar el backup: " + e.getMessage());
+        }
     }
+
     public static void buckUpIncrementalNio() {
-        SimpleDateFormat formatoDate = new SimpleDateFormat("yy.MM.dd_HH.mm.ss");
-        String date = formatoDate.format(new Date());
+        try {
+            SimpleDateFormat formatoDate = new SimpleDateFormat("yy.MM.dd_HH.mm.ss");
+            String date = formatoDate.format(new Date());
 
-        /*DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy.MM.dd_HH.mm.ss").withZone(ZoneId.systemDefault());
-        Instant instant = Instant.parse("2022-02-15T18:35:24.00Z");
-        String formattedInstant = formatter.format(instant);*/
-
-        String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
-        Path rutaOrigen = Path.of(rutaOrigenString);
-        String rutaDestinoString = Leer.pedirCadena("Introduce ruta archivo destino: ");
-        Path rutaDestino = Path.of(rutaDestinoString + date);
-        copiaBuckUp(rutaOrigen, rutaDestino);
+            String rutaOrigenString = Leer.pedirCadena("Introduce ruta archivo original: ");
+            Path rutaOrigen = Path.of(rutaOrigenString);
+            String rutaDestinoString = Leer.pedirCadena("Introduce ruta carpeta de backups incrementales: ");
+            Path rutaDestino = Path.of(rutaDestinoString + "/" + date);
+            copiaBuckUp(rutaOrigen, rutaDestino);
+        } catch (IOException e) {
+            System.err.println("Error al realizar el backup incremental: " + e.getMessage());
+        }
     }
-    public static void copiaBuckUp(Path po, Path pd){
 
-        // Crear la carpeta de destino si no existe
-        if (pd.toFile().mkdir()) {
+    public static void copiaBuckUp(Path po, Path pd) throws IOException {
+        //creo la carpeta de destino si no existe
+        if (Files.notExists(pd)) {
+            Files.createDirectories(pd);
             //DirectoryStream<Path> es un objeto para iterar sobre un directorio y asi poder usar un for:each
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(po)) {
-                // Iterar sobre los archivos en la carpeta de origen
+                // itero sobre los archivos en la carpeta de origen
                 for (Path archivo : stream) {
-                    //si el archivo de origen existe, el de destino no existe y su nombre no es igual al nombre de destino
-                    if (Files.exists(po) && !Files.exists(pd) && !po.getFileName().equals(pd.getFileName())) {
-                        //entonces hace la copia
-                        Files.copy(archivo, pd.resolve(archivo.getFileName()));
+                    Path destinoArchivo = pd.resolve(archivo.getFileName());
+
+                    // compruebo si el archivo ya existe en la anterior copia o ha sido modificado
+                    if (Files.notExists(destinoArchivo) || Files.getLastModifiedTime(archivo).compareTo(Files.getLastModifiedTime(destinoArchivo)) > 0) {
+                        Files.copy(archivo, destinoArchivo, StandardCopyOption.REPLACE_EXISTING);
+                    }else{
+                        System.out.println("El archivo '" + archivo.getFileName() + "' ya existe o no ha sido modificado en backups anteriores");
                     }
                 }
-            } catch (SecurityException e) {
+            }catch (SecurityException e) {
                 System.err.println("No tiene permiso de lectura: " + e.getMessage());
             } catch (IOException e) {
                 System.err.println("Error al copiar el backup: " + e.getMessage());
