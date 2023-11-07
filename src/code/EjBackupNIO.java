919ed33d50a221b4d27eea84e5b9f1d77e1f8453
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-11-07 11:04:07 +0100

    commit

--- src/code/EjBackupNIO.java
+++ src/code/EjBackupNIO.java
@@ -1,7 +1,13 @@
 package code;
 
+import libs.FicheroEscribible;
 import libs.Leer;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.text.SimpleDateFormat;
@@ -9,6 +15,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static libs.FicheroEscribible.ficheroEscribible;
+import static libs.FicheroEscribible.ficheroLegible;
 
 public class EjBackupNIO {
 
@@ -30,54 +39,55 @@ public class EjBackupNIO {
     }
     public static void copiaCompleta(Path po, Path pd) {
         //creo la carpeta de destino si no existe
-        if (Files.notExists(pd)) {
-            try {
-                Files.createDirectories(pd);
-            } catch (IOException e) {
-                System.err.println("Error al crear el directorio de destino");
+        try {
+            if (ficheroEscribible(pd)) {
+                //DirectoryStream<Path> es un objeto para iterar sobre un directorio y asi poder usar un for:each
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(po)) {
+                    // itero sobre los archivos en la carpeta de origen y hago una copia total
+                    for (Path archivo : stream) {
+                        if(ficheroLegible(po)) {
+                            Path destinoArchivo = pd.resolve(archivo.getFileName());
+                            Files.copy(archivo, destinoArchivo, StandardCopyOption.REPLACE_EXISTING);
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error al copiar el backup: " + e.getMessage());
+                }
             }
-        }
-        //DirectoryStream<Path> es un objeto para iterar sobre un directorio y asi poder usar un for:each
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(po)) {
-            // itero sobre los archivos en la carpeta de origen y hago una copia total
-            for (Path archivo : stream) {
-                Path destinoArchivo = pd.resolve(archivo.getFileName());
-                Files.copy(archivo, destinoArchivo, StandardCopyOption.REPLACE_EXISTING);
-            }
-        } catch (IOException e) {
-            System.err.println("Error al copiar el backup: " + e.getMessage());
+        } catch (SecurityException e) {
+            System.err.println("No tiene permiso de lectura: " + e.getMessage());
         }
     }
 
     public static void copiaIncremental(Path po, Path pc, Path pd) {
         //creo la carpeta de destino si no existe
-        if (Files.notExists(pd)) {
-            try {
-                Files.createDirectories(pd);
-            } catch (IOException e) {
-                System.err.println("Error al crear el directorio de destino");
-            }
-        }
+        try {
+            if (ficheroEscribible(pd)) {
+                //listo los archivos de la carpeta original
+                try (DirectoryStream<Path> stream = Files.newDirectoryStream(po)) {
+                    for (Path pOriginal : stream) {
 
-        //listo los archivos de la carpeta original
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(po)) {
-            for (Path pOriginal : stream) {
-                Path archivoOriginal = po.relativize(pOriginal);
-                Path archivoCompleto = pc.resolve(archivoOriginal);
-                Path archivoIncremental = pd.resolve(archivoOriginal);
+                        if(ficheroLegible(po)) {
+                            Path archivoOriginal = po.relativize(pOriginal);
+                            Path archivoCompleto = pc.resolve(archivoOriginal);
+                            Path archivoIncremental = pd.resolve(archivoOriginal);
 
-                //comparo el archivo original y el completo para comprobar si existe un nuevo archivo. Si no existe el archivo solo saca un mensaje de que no hay archivos nuevos
-                if (Files.exists(archivoCompleto) && Files.getLastModifiedTime(pOriginal).compareTo(Files.getLastModifiedTime(archivoCompleto)) <= 0) {
-                    System.out.println("El archivo '" + archivoOriginal.getFileName() + "' ya existe o no ha sido modificado desde la última copia");
-                    continue;
+                            //comparo el archivo original y el completo para comprobar si existe un nuevo archivo. Si no existe el archivo solo saca un mensaje de que no hay archivos nuevos
+                            if (Files.exists(archivoCompleto) && Files.getLastModifiedTime(pOriginal).compareTo(Files.getLastModifiedTime(archivoCompleto)) <= 0) {
+                                System.out.println("El archivo '" + archivoOriginal.getFileName() + "' ya existe o no ha sido modificado desde la última copia");
+                            }else {
+                                //copio el archivo nuevo desde la carpeta original a la carpeta incremental
+                                Files.copy(pOriginal, archivoIncremental, StandardCopyOption.REPLACE_EXISTING);
+                                System.out.println("Se ha copiado: " + archivoOriginal.getFileName());
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Error al realizar la copia incremental: " + e.getMessage());
                 }
-
-                //copio el archivo nuevo desde la carpeta original a la carpeta incremental
-                Files.copy(pOriginal, archivoIncremental, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("Se ha copiado: " + archivoOriginal.getFileName());
             }
-        } catch (IOException e) {
-            System.err.println("Error al realizar la copia incremental: " + e.getMessage());
+        } catch (SecurityException e) {
+            System.err.println("No tiene permiso de lectura: " + e.getMessage());
         }
     }
 }
